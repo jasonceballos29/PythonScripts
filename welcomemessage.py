01322537bd934db24fd61b3f62d93e85905fb618
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-09 17:42:05 -0400

    Create welcomemessage.py
    Generates a welcome message displaying the users first and last names.

---
+++ welcomemessage.py
@@ -0,0 +1,8 @@
+# Allow user to input their First Name and assign that input to a variable
+firstname = input("What is your first name? ")
+
+# Allow user to input their Last Name and assign that input to a variable
+lastname = input("What is your last name? ")
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+Your name is FIRSTNAME AND LASTNAME

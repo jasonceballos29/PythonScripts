13f3f25885cfaa56534068d481020a948fe4ef00
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-14 15:03:03 -0400

    AWS Inventory script

---
+++ Lengthofstring.py
@@ -0,0 +1,6 @@
+demoList = ["Python", 1, "JavaScript", True, "HTML", "CSS", 22]
+
+sizeOfDemoList = len(demoList)
+
+print("The length of the list using the len() method is: " + str(sizeOfDemoList))
+# Output: The length of the list using the len() method is: 7 

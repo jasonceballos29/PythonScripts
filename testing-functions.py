51e9dcfc94209122e0b60f4fd26b6852352cda35
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-21 15:13:45 -0400

    Creating script to test functions

---
+++ testing-functions.py
@@ -0,0 +1,15 @@
+# 1) Write a `split_name` function that takes a string and returns a dictionary with first_name and last_name
+
+def split_name(name):
+    names = name.split()
+    first_name = names[0]
+    last_name = names[-1]
+    return {
+        'first_name': first_name,
+        'last_name': last_name,
+    }
+
+assert split_name("Kevin Bacon") == {
+    "first_name": "Kevin",
+    "last_name": "Bacon",
+}, f"Expected {{'first_name': 'Kevin', 'last_name': 'Bacon'}} but received {split_name('Kevin Bacon')}"

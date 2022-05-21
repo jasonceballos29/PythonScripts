47339f53023dec04c7f1bd5077e2d81a530bc946
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-21 19:20:30 -0400

    Updated code

--- Week 14/Project/EC2RandonNameGenerator.py
+++ Week 14/Project/EC2RandonNameGenerator.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3.7
+
 #EC2 Random Name Generator
 #Authored by Jason Ceballos
 #05/18/2022
+# Several departments share an AWS environment. You need to ensure that the EC2s are properly named and are unique so team members can easily tell who the EC2 instances belong to. 
+# Use Python to create your unique EC2 names that the users can then attach to the instances. The Python Script should:
+
+import random
+import string
+
+departments = ["Marketing", "Accounting", "FinOps"]
+
+# 1. Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
+
+ec2_amount = int(input("Please enter how many EC2 instance names you need: "))
+
+# 2. Allow the user to input the name of their department that is used in the unique name.
+
+dept_name = str(input("Please enter your department name: "))
+
+# 3. Generate random characters and numbers that will be included in the unique name.
+
+randomstring = ''.join(dept_name + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(24))
+print(randomstring)

8a0993ec8b9d3d82932812e3bc743af473503ba4
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-22 16:48:22 -0400

    Finalizing script with logic to check for specific tags to remove

--- Week 15/Project/shutdownec2.py
+++ Week 15/Project/shutdownec2.py
@@ -1,14 +1,40 @@
-import sys
-import boto3
+# Stop EC2 Instances Python Script
+# Also checks for Specific Tag before shutting down
+# Authored by Jason Ceballos
+# 05/22/22
 
-# Boto3
-# Use the filter() method of the instances collection to retrieve
-# all running EC2 instances.
-response = client.stop_instances(
-    InstanceIds=[
-        'string',
-    ],
-    Hibernate=True|False,
-    DryRun=True|False,
-    Force=True|False
-)
+import boto3 #import AWS boto3 library
+region='us-east-1'
+instance_state = 'running'
+ec2 = boto3.client('ec2', region_name=region)
+#ec2 = boto3.resource('ec2',"us-east-1") 
+
+# Filter all running instances
+
+instances = ec2.instances.all(Filters=[{'Name': 'instance-state-name', 'Values': [instance_state]}])
+
+# Declared list to store running instances
+all_running_instances = []
+specific_tag = 'Dev'  
+for instance in instances:
+    
+    # store all running instances
+     all_running_instances.append(instance)
+        
+    # Instances with specific tags
+     if instance.tags != None:
+         for tags in instance.tags:
+             
+            # Instances with tag 'Dev'
+             if tags["Key"] == specific_tag:
+                
+                # Remove instances with specfic tags from all running instances
+                 all_running_instances.remove(instance)
+                    
+#print(all_running_instances)
+for specific in all_running_instances:
+    print(f'Stopping EC2 instance: {specific.id}')
+    specific.wait_until_stopped()
+    print(f'EC2 instance "{specific.id}" has been stopped')
+    # print(specific)
+    # specific.stop()

947fac778f340da9b112f6af3482189e3ae07ef0
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-22 01:18:10 -0400

    Final updates to script. Fixed loop issue on generator and elif as well

--- Week 14/Project/EC2RandonNameGenerator.py
+++ Week 14/Project/EC2RandonNameGenerator.py
@@ -21,19 +21,24 @@ print("Welcome to the EC2 Random Name Generator!")
 # 1. Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
 
 ec2_amount = int(input("Please enter how many EC2 instance names you need: "))
+raw_ec2_amount = int(ec2_amount)
 
 # 2. Allow the user to input the name of their department that is used in the unique name.
 
-departments = ("Marketing", "Accounting", "FinOps")
+departments = ['Marketing', 'Accounting', 'FinOps']
 dept_name = str(input("Please enter your department name: "))
+
 print("Your Department is:" , dept_name)
 # We only want users from the departments list we provided to use this program. Everyone else gets the print command and program exits
 
-while ec2_amount >= 1:
-    for num in range(ec2_amount):
-        if dept_name != list(departments):
+count = 1
+while count <= ec2_amount:
+    for department in departments:
+        if dept_name in departments:
             print("Random EC2 Name is = ", dept_name + getEC2NameString(18))
-        continue
-else: 
-    print("You do not have permission to run this program. Have a nice day!")
-    exit()
+            count += 1
+        elif dept_name != list(departments):
+            print("You do not have permission to run this program. Have a nice day!")
+            exit()
+exit()
+

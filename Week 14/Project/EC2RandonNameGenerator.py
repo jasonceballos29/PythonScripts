6c308dbe8f900fafbc94376a3b33add06c66a21d
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-05-21 23:37:10 -0400

    Updating script to include logic for iterations

--- Week 14/Project/EC2RandonNameGenerator.py
+++ Week 14/Project/EC2RandonNameGenerator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3.7
 
-#EC2 Random Name Generator
-#Authored by Jason Ceballos
-#05/18/2022
+# EC2 Random Name Generator
+# Authored by Jason Ceballos
+# 05/18/2022
 # Several departments share an AWS environment. You need to ensure that the EC2s are properly named and are unique so team members can easily tell who the EC2 instances belong to. 
 # Use Python to create your unique EC2 names that the users can then attach to the instances. The Python Script should:
 
+import numbers
 import random
 import string
 
-departments = ["Marketing", "Accounting", "FinOps"]
+# Generate random characters and numbers that will be included in the unique name.
+
+def getEC2NameString(length):
+    """Generate a random string"""
+    str = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    return ''.join(random.choice(str) for i in range(length))
+
+print("Welcome to the EC2 Random Name Generator!")
 
 # 1. Allow the user to input how many EC2 instances they want names for and output the same amount of unique names.
 
@@ -17,9 +25,16 @@ ec2_amount = int(input("Please enter how many EC2 instance names you need: "))
 
 # 2. Allow the user to input the name of their department that is used in the unique name.
 
+departments = ["Marketing", "Accounting", "FinOps"]
 dept_name = str(input("Please enter your department name: "))
+print("Your Department is:" , dept_name)
+# We only want users from the departments list we provided to use this program. Everyone else gets the print command and program exits
 
-# 3. Generate random characters and numbers that will be included in the unique name.
-
-randomstring = ''.join(dept_name + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(24))
-print(randomstring)
+while ec2_amount >= 1:
+    for num in range(ec2_amount):
+        if dept_name != list(departments):
+            print("Random EC2 Name is = ", dept_name + getEC2NameString(18))
+        continue
+else: 
+    print("You do not have permission to run this program. Have a nice day!")
+    exit()
